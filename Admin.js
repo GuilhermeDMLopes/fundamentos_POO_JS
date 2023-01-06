30eaa68ac4bc36a04dc1038e2d798ff69751a24b
guilherme <guilhermedmlopes@hotmail.com>
2023-01-06 12:24:57 -0300

    Adding files and info about Encapsuling

--- Admin.js
+++ Admin.js
@@ -1,25 +1,17 @@
-//Fazendo herança de classe
 import User from './User.js';
 
-class Admin extends User {
-    //Se vamos reaproveitar a classe Mãe, porque colocar construtor?
-    //POis nem sempre vamos querer reaproveitar tudo da classe mãe
-    //Passamos no construtor e na classe super apenas o que queremos usar para a classe
+export default class Admin extends User {
+    
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {
-        //Como vamos reaproveitar tudo que está em usuario, fazemos da seguinte forma
+        
         super(nome, email, nascimento, role, ativo)
     }
 
-    //Método particular da subclasse de User
+    nomeAdmin() {
+        return `${this.nome}`
+    }
+    
     criarCurso(nomeDoCurso, vagas) {
         return `${nomeDoCurso}, criado com ${vagas} vagas`
     }
 }
-
-const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2023-01-03')
-console.log(novoAdmin)
-//Como herdamos de User, temos acesso as propriedades e aos métodos de classe mãe
-console.log(novoAdmin.exibirInfos())
-console.log(novoAdmin.criarCurso('JS', 20))
-
-//Classes não possuem hoisting!!
--- User.js
+++ User.js
@@ -1,30 +1,130 @@
-// Arquivo explicando conceito de classe um JS
-//Como observado, arquivos de classes devem inciar em letra maiúscula
-//Seguindo o diagrama UML:
+// Arquivo explicando conceito de encapsulamento das propriedades e métodos de uma classe
+//Protegendo dados que não devem ser acessados/manipulados por fora da classe
 
-//Export default para quando for trabalhar com heranças de classes
 export default class User {
-    //construtor de classe
-    //ativo ja pode ser começado como padrão como true
+    //Declarado uma propriedade privada. Deve ser feita antes do construtor
+    #nome
+    #email
+    #nascimento
+    #role
+    #ativo
     constructor(nome, email, nascimento, role, ativo = true) {
-        this.nome = nome
-        this.email = email
-        this.nascimento = nascimento
-        //Vai receber um parametro, se não receber é setado como estudante
-        this.role = role || 'estudante'
-        this.ativo = ativo
+        this.#nome = nome
+        this.#email = email
+        this.#nascimento = nascimento        
+        this.#role = role || 'estudante'
+        this.#ativo = ativo
     }
 
-    //Na classe, não precisamos criar funções para definir métodos
+    //Criando Getter
+    //Criamos com o mesmo nome da variavel para o usuário não perceber que se trata de uma função getter
+    get nome() {
+        //Pode ter lógicas condicionais
+        return this.#nome
+    }
+
+    get email() {
+        return this.#email
+    }
+
+    get nascimento() {
+        return this.#nascimento
+    }
+
+    get role() {        
+        return this.#role
+    }
+
+    get ativo() {        
+        return this.#ativo
+    }
+
+    //Criando Setters
+    //Criamos com o mesmo nome da variavel para o usuário não perceber que se trata de uma função setter
+    set nome(novoNome) {
+        //Pode ter lógicas condicionais
+        if(novoNome === ''){
+            throw new Error('Formato inválido')
+        }
+        return this.#nome = novoNome
+    }
+
+    set email(novoEmail) {
+        return this.#email = novoEmail
+    }
+
+    set nascimento(novoNascimento) {
+        return this.#nascimento = novoNascimento
+    }
+
+    set role(novoRole) {        
+        return this.#role = novoRole
+    }
+
+    set ativo(novoAtivo) {        
+        return this.#ativo = novoAtivo
+    }
+    
+    //Declarando método privado
+    #montaObjUser() {
+        //Para retornar um objeto, devemos usar a seguinte sintaxe
+        return ({
+            nome: this.#nome,
+            email: this.#email,
+            nascimento: this.#nascimento,
+            role: this.#role,
+            ativo: this.#ativo
+        })
+    }
+
     exibirInfos() {
+        //return `${this.#nome}, ${this.#email}`
+        /*Forma de retornar um método privado
+        const objUser = this.#montaObjUser()
+        return `${objUser.nome}, ${objUser.email}`*/
+        //Depois dos getters e setters, o this recebe o contexto de get e set
         return `${this.nome}, ${this.email}`
     }
 }
 
-/*
-// instanciando a classe
-const novoUser = new User('Guilherme', 'g@g.com', '2023-01-03')
-console.log(novoUser)
-console.log(novoUser.exibirInfos())
-console.log(User.prototype.isPrototypeOf(novoUser)) //Tem que retornar true
+/* Antigamente não existia a sintaxe #. Para declarar propriedades privadas, 
+usáva-se "_" antes da variavel.
+*/
+
+/*Exemplo de Uso de Getters e Setters
+Preciso juntar nome e sobrenome de um usuario e devem ser propriedades distintas
+default class User {
+ #nome
+ #sobrenome
+  constructor (nome, sobrenome, email, nascimento, role, ativo = true) {
+   this.#nome = nome
+   this.#sobrenome = sobrenome
+   // restante das propriedades
+  }
+
+   set nome(novoNome) {
+   if (novoNome === '') {
+     throw new Error('formato não válido')
+   }
+   let [nome, ...sobrenome] = novoNome.split(" ")
+   sobrenome = sobrenome.join(' ')
+   this.#nome = nome
+   this.#sobrenome = sobrenome
+ }
+
+  get nome() {
+   return this.#nome
+ }
+
+ get sobrenome() {
+   return this.#sobrenome
+ }
+
+}
+
+const novoUser = new User('Juliana', 'Souza', 'j@j.com', '2021-01-01')
+console.log(novoUser.nome) //'Juliana'
+novoUser.nome = 'Juliana Silva Souza'
+console.log(novoUser.nome) //'Juliana'
+console.log(novoUser.sobrenome) //'Silva Souza'
 */
